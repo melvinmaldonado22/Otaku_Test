df9c8dd4f9595bbf6e6b5fdc9b50f79a2e142f07
melvinmaldonado22 <xmelvinymaldonadozmoncada@gmail.com>
2023-08-10 14:41:12 -0600

    user
    change ur user auth

--- src/controllers/usuario-controller.ts
+++ src/controllers/usuario-controller.ts
@@ -4,6 +4,12 @@ import usuarioModel from '../models/usuario';
 const authUser = async function(req:Request, res:Response){
     try{
         const contraseña = req.body.contraseña;
+        if(!req.body.usuario){
+            return res.json({
+                auth: false,
+                message: 'Ingrese un usuario válido'
+            })
+        }
         const usuario = await usuarioModel.find({usuario:req.body.usuario});
         if(usuario[0].contraseña===contraseña){
             return res.json({
